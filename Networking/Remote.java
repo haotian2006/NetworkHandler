fbf1adac111e5cb9d1e45b1f1f58c73f919b854c
Haotian2006 <71573739+haotian2006@users.noreply.github.com>
2025-03-31 18:53:55 -0700

    Update Remote.java

--- Networking/Remote.java
+++ Networking/Remote.java
@@ -5,27 +5,70 @@ import java.io.IOError;
 import java.io.IOException;
 import java.util.function.Function;
 
-import Networking.Signal.*;
-// THIS CAN BE OPTIMIZED FURTHER IF WE SEND A UNIQUE REMOTE ID THATS AN UINT8 FOR EACH REMOTE
+import Signal.*;
+// THIS CAN BE OPTIMIZED FURTHER IF WE SEND A UNIQUE REMOTE ID THATS AN UINT8 FOR EACH REMOTE (DONE!)
+
+/**
+ * Used to for communications
+ * @author haotian
+ */
 public abstract class Remote {
+    /**
+     * Name of the remote
+     */
     public final String name;
+
+    /**
+     * a event that is fired when a message is retrieved
+     */
     protected Signal<Packet> onMessageSignal;
     public final Event<Packet> onMessage;
 
 
+
+    /**
+     * @param name name of the remote
+     */
     protected Remote(String name) {
         this.name = name;
         onMessageSignal = new Signal<Packet>();
         onMessage = onMessageSignal.event;
     }
 
-    public abstract void fireServer(Packet data) throws IOException;
-    
-    public abstract void fireClient(NetworkUser client, Packet data) throws IOException;
 
-    public abstract void fireAllClients(Packet data) throws IOException;
+    /**
+     * Sends a message to the Server
+     * @param data the packet to send
+     */
+    public abstract void fireServer(Packet data);
 
-    public abstract void fireAllClientsExcept(NetworkUser exclude,Packet data) throws IOException;
+    /**
+     * Sends a message to the given Client
+     * @param client the client to send to
+     * @param data the packet to send
+     */
+    public abstract void fireClient(NetworkUser client, Packet data);
+
+    /**
+     * Sends a message to all the Clients
+     * @param data the packet to send
+     */
+    public abstract void fireAllClients(Packet data);
+
+    /**
+     * Sends a message to all the Clients except one
+     * @param exclude the client to exclude
+     * @param data the packet to send
+     */
+    public abstract void fireAllClientsExcept(NetworkUser exclude,Packet data);
+
+    /**
+     * Returns the name of the remote in a string format
+     * @return the name of the remote
+     */
+    public String toString() {
+        return name;
+    }
     
 }
 
